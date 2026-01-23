6ed85dfaeaf830acf04cc11ffc55735c4326b727
Praveen Kumar <praveenbaghelmaurya@gmail.com>
2026-01-23 14:22:39 +0530

    feat: Add user dashboard page with event carousels and global CSS theming.

--- app/(root)/user/page.tsx
+++ app/(root)/user/page.tsx
@@ -6,9 +6,21 @@ import Footer from "@/components/Footer/Footer";
 const UserDashboardpage = () => {
   return (
     <>
+      {/* stories */}
       <div className="overflow-hidden font-extralight">
         <CarouselDemo />
         <div className="mx-4 mt-10 h-1/4 w-full font-sans">
+          <div className="flex items-center justify-between">
+            <h1 className="text-2xl">Featured Events</h1>
+            <h1 className="mx-12 cursor-pointer text-2xl hover:font-light">
+              View All
+            </h1>
+          </div>
+          <CarouselSpacing />
+        </div>
+
+        {/* feature events */}
+        <div className="mx-4 mt-10 w-full font-sans">
           <div className="flex items-center justify-between">
             <h1 className="text-2xl">Upcoming Events</h1>
             <h1 className="mx-12 cursor-pointer text-2xl hover:font-light">
@@ -17,28 +29,31 @@ const UserDashboardpage = () => {
           </div>
           <CarouselSpacing />
         </div>
-
-        <div className="mx-4 mt-10 w-full font-sans">
-          <div className="flex items-center justify-between">
-            <h1 className="text-2xl">Popular Events</h1>
-            <h1 className="mx-12 cursor-pointer text-2xl hover:font-light">
-              View All
-            </h1>
-          </div>
-          <CarouselSpacing />
-        </div>
       </div>
 
+      {/* popular events */}
       <div className="mx-4 mt-10 w-full font-sans">
         <div className="flex items-center justify-between">
-          <h1 className="text-2xl">Popular Events</h1>
+          <h1 className="text-2xl">Past Popular Events</h1>
           <h1 className="mx-12 cursor-pointer text-2xl hover:font-light">
             View All
-          </h1> 
+          </h1>
+        </div>
+        <CarouselSpacing />
+      </div>
+
+      {/* past popular events */}
+      <div className="mx-4 mt-10 w-full font-sans">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl">Past Popular Events</h1>
+          <h1 className="mx-12 cursor-pointer text-2xl hover:font-light">
+            View All
+          </h1>
         </div>
         <CircularCarousel />
       </div>
 
+      {/* footer */}
       <div className="mx-4 mt-10 w-full font-sans">
         <div className="mx-4 mt-10 w-full font-sans">
           <Footer />
